04e123c0edab448208eca19a62d517a70de8c357
TS <thanyawat.s@7solutions.co.th>
2024-02-14 18:54:12 +0700

    refactor: new something

--- main.go
+++ main.go
@@ -19,13 +19,13 @@ func main() {
 	//redisClient := redisInfra.InitRedis(cfg.Redis)
 	app := fiber.New()
 	app.Get("/version", func(c *fiber.Ctx) error {
-		return c.SendString("v1.1.1")
+		return c.SendString("v1.1.3")
 	})
 	//app.Post("/api/register", register_handler.RegisterHandler(register_handler.NewRegisterFunc(db), register_handler.NewRegisterRedisFunc(redisClient)))
 	//app.Post("/api/verify", register_handler.SetVerifyHandler(register_handler.NewSetVerifyFunc(redisClient), register_handler.NewDelVerifyFunc(redisClient), register_handler.NewUpdateVerifyFunc(db)))
 	//app.Get("/api/:cid", register_handler.GetStatusHandler(register_handler.NewGetStatusFunc(db)))
-	app.Get("/hello-world", func(c *fiber.Ctx) error {
-		return c.SendString("Hello World")
+	app.Get("/something", func(c *fiber.Ctx) error {
+		return c.SendString("something")
 	})
 
 	err = app.Listen(cfg.Server.Port)
